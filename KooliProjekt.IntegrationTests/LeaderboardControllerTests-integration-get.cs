60d8779249e24c77362c71f6a35630adcdcb5dbc
ivnGra <ivan.gratsov@tptlive.ee>
2025-04-04 09:35:00 +0300

    added controllers get -method tests

---
+++ KooliProjekt.IntegrationTests/LeaderboardControllerTests-integration-get.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using KooliProjekt.Data;
+using KooliProjekt.IntegrationTests.Helpers;
+using Xunit;
+using static NuGet.Packaging.PackagingConstants;
+
+namespace KooliProjekt.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class LeaderboardControllerTests : TestBase
+    {
+        private readonly HttpClient _client;
+        private readonly ApplicationDbContext _context;
+
+        public LeaderboardControllerTests()
+        {
+            _client = Factory.CreateClient();
+            _context = (ApplicationDbContext)Factory.Services.GetService(typeof(ApplicationDbContext));
+        }
+
+        [Fact]
+        public async Task Index_should_return_correct_response()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Leaderboards");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_was_not_found()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Leaderboards/Details/100");
+
+            // Asserta
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Leaderboards/Details/");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Details_should_return_ok_when_list_was_found()
+        {
+            // Arrange
+            var list = new Leaderboard { Name = "test", Place = "test" };
+            _context.Leaderboards.Add(list);
+            _context.SaveChanges();
+
+            // Act
+            using var response = await _client.GetAsync("/Leaderboards/Details/" + list.Id);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ KooliProjekt.IntegrationTests/TeamControllerTests-integration-get.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using KooliProjekt.Data;
+using KooliProjekt.IntegrationTests.Helpers;
+using Xunit;
+using static NuGet.Packaging.PackagingConstants;
+
+namespace KooliProjekt.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class TeamControllerTests : TestBase
+    {
+        private readonly HttpClient _client;
+        private readonly ApplicationDbContext _context;
+
+        public TeamControllerTests()
+        {
+            _client = Factory.CreateClient();
+            _context = (ApplicationDbContext)Factory.Services.GetService(typeof(ApplicationDbContext));
+        }
+
+        [Fact]
+        public async Task Index_should_return_correct_response()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Teams");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_was_not_found()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Teams/Details/100");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Teams/Details/");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Details_should_return_ok_when_list_was_found()
+        {
+            // Arrange
+            var list = new Team { TeamName = "Test", TeamDescription = "Test"};
+            _context.Teams.Add(list);
+            _context.SaveChanges();
+
+            // Act
+            using var response = await _client.GetAsync("/Teams/Details/" + list.Id);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
